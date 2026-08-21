154df5d93dac8eb4d36f1e4fdf3667b955a39b54
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 12:28:44 +0200

    Migrate from SuppressedAssociatedTypes

--- Package.swift
+++ Package.swift
@@ -141,7 +141,6 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
         .enableExperimentalFeature("LifetimeDependence"),
         .enableExperimentalFeature("Lifetimes"),
-        .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableUpcomingFeature("InferIsolatedConformances"),
     ]
 
